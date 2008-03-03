7ee50d53b8642a93f54bbea984e666e47df49514
Markus Hoenicka <markus.hoenicka@mhoenicka.de>
2008-03-03 20:04:16 +0000

    fixed _dbivs. _dbd_internal_error_handler typo

--- drivers/sqlite3/dbd_sqlite3.c
+++ drivers/sqlite3/dbd_sqlite3.c
@@ -22,7 +22,7 @@
  * Copyright (C) 2005-2007, Markus Hoenicka <mhoenicka@users.sourceforge.net>
  * http://libdbi-drivers.sourceforge.net
  * 
- * $Id: dbd_sqlite3.c,v 1.21 2008/02/17 16:29:51 mhoenicka Exp $
+ * $Id: dbd_sqlite3.c,v 1.20.2.1 2008/03/03 20:04:16 mhoenicka Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -431,7 +431,6 @@ dbi_result_t *dbd_list_tables(dbi_conn_t *conn, const char *db, const char *patt
    */
   dbi_result_t *dbi_result;
   dbi_conn_t* tempconn;
-  dbi_inst instance;
   int retval;
   char* sq_errmsg;
   char* sql_cmd;
@@ -439,8 +438,7 @@ dbi_result_t *dbd_list_tables(dbi_conn_t *conn, const char *db, const char *patt
   /* this function tries to query a specific database, so we need a
    separate connection to that other database, retrieve the table names,
    and feed them to a temporary table in our main connection */
-  instance = dbi_driver_get_instance(dbi_conn_get_driver(conn));
-  tempconn = dbi_conn_new_r("sqlite3", instance);
+  tempconn = dbi_conn_new("sqlite3");
 
   /* we explicitly cast to (char*) as we discard the "const" thing here */
   dbi_conn_set_option(tempconn, "dbname", (char*)db);
@@ -733,7 +731,7 @@ int find_result_field_types(char* field, dbi_conn_t *conn, const char* statement
 				      &errmsg);
 
   if (query_res || !table_numrows) {
-    _dbi_internal_error_handler(conn, NULL, DBI_ERROR_BADNAME);
+    _dbd_internal_error_handler(conn, NULL, DBI_ERROR_BADNAME);
     /*       printf("field not found\n"); */
     return 0;
   }
